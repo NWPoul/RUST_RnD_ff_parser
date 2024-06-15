b5b84b4d90b151151a02d17db2a1e6b8d5a20362
Pavel <nwpoul@yandex.ru>
2024-06-15 12:29:02 +0300

    w

--- src/utils/u_serv.rs
+++ src/utils/u_serv.rs
@@ -15,22 +15,22 @@ pub fn abs_max(f_prev: f64, f_new: f64) -> f64 {
     f_prev.abs().max(f_new.abs())
 }
 
-struct Vector {
+pub struct Vector {
     x: f64,
     y: f64,
     z: f64,
 }
 
 impl Vector {
-    fn magnitude(&self) -> f64 {
+    pub fn magnitude(&self) -> f64 {
         f64::sqrt(self.x.powi(2) + self.y.powi(2) + self.z.powi(2))
     }
 
-    fn dot_product(a: &Vector, b: &Vector) -> f64 {
+    pub fn dot_product(a: &Vector, b: &Vector) -> f64 {
         a.x * b.x + a.y * b.y + a.z * b.z
     }
 
-    fn angle_between(a: &Vector, b: &Vector) -> f64 {
+    pub fn angle_between(a: &Vector, b: &Vector) -> f64 {
         let cos_theta = Self::dot_product(a, b) / (a.magnitude() * b.magnitude());
         cos_theta.acos()
     }
