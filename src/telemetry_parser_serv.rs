0849170e43cd6d6fc6d48feedf3396cc64c75d6b
Pavel <nwpoul@yandex.ru>
2024-08-26 19:41:30 +0300

    w-

--- src/telemetry_parser_serv.rs
+++ src/telemetry_parser_serv.rs
@@ -76,15 +76,19 @@ pub fn parse_telemetry_from_mp4_file(input_file: &str) -> Result<TelemetryParsed
 
     let input = Input::from_stream(&mut stream, filesize, &opts.input, |_|(), Arc::new(AtomicBool::new(false))).unwrap();
 
-    let cam_info = format!("{} {}", input.camera_type(), input.camera_model().unwrap_or(&"".into()));
+    let cam_info = format!("{} {}",
+        input.camera_type(),
+        input.camera_model().unwrap_or(&"".into()),
+    );
     println!("Detected camera: {cam_info}");
 
     let samples = match input.samples.as_ref() {
         Some(samples_ref) => samples_ref,
         None => {return Err(format!("NO_SAMPLES!"))},
     };
+    get_iso_data(&samples);
 
-    if opts.dump { dump_samples(samples);}
+    if opts.dump { dump_samples(samples)}
 
     let imu_data = match util::normalized_imu(&input, opts.imuo) {
         Ok(data) => data,
@@ -121,4 +125,48 @@ pub fn get_result_metadata_for_file(input_file: &str) -> Result<TelemetryParsedD
     })
 }
 
+
+
+
+
+
+
+pub fn get_iso_data(samples: &Vec<util::SampleInfo>) {
+    for info in samples {
+        if info.tag_map.is_none() { continue; }
+
+        let grouped_tag_map = info.tag_map.as_ref().unwrap();
+
+        for (group, map) in grouped_tag_map {
+            if group == &GroupId::Exposure {
+                // dbg!(map.get(&TagId::Data));
+            };
+            if group == &GroupId::Custom("SensorISO".to_string()) {
+                if let Some(taginfo) = map.get(&TagId::Data) {
+                    match &taginfo.value {
+                        // Sony and GoPro
+                        TagValue:: arr) => {
+                            let arr = arr.get();
+                            let reading_duration = info.duration_ms / arr.len() as f64;
+                            fix_timestamps = true;
+
+                            for (j, v) in arr.iter().enumerate() {
+                                if final_data.len() <= data_index + j {
+                                    final_data.resize_with(data_index + j + 1, Default::default);
+                                    final_data[data_index + j].timestamp_ms = timestamp;
+                                    timestamp += reading_duration;
+                                }
+                                let itm = v.clone().into_scaled(&raw2unit, &unit2deg).orient(io);
+                                     if group == &GroupId::Gyroscope     { final_data[data_index + j].gyro = Some([ itm.x, itm.y, itm.z ]); }
+                                else if group == &GroupId::Accelerometer { final_data[data_index + j].accl = Some([ itm.x, itm.y, itm.z ]); }
+                                else if group == &GroupId::Magnetometer  { final_data[data_index + j].magn = Some([ itm.x, itm.y, itm.z ]); }
+                            }
+                        },
+                        _ => dbg!()
+                    }
+                }
+            }
+        }
+    }
+}
 // let telemetry_sma_acc_data = get_sma_list(&telemetry_xyz_acc_data, 100);
